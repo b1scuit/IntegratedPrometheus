48ecadab879f70a1571b14d841a7878282fa6fc2
B1scuit <dysvir@gmail.com>
2025-01-20 13:43:19 +0000

    Fixes

--- src/main/java/org/b1scuit/integratedprometheus/prometheus/Prometheus.java
+++ src/main/java/org/b1scuit/integratedprometheus/prometheus/Prometheus.java
@@ -6,16 +6,19 @@ import io.prometheus.metrics.instrumentation.jvm.JvmMetrics;
 import org.slf4j.Logger;
 
 import java.io.IOException;
+import java.net.InetAddress;
 
 public class Prometheus {
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public void initialize() throws InterruptedException, IOException{
         JvmMetrics.builder().register();
+        InetAddress inetAddress = InetAddress.getByName("0.0.0.0");
 
         HTTPServer server = HTTPServer.builder()
+                .inetAddress(inetAddress)
                 .port(9400).buildAndStart();
-        LOGGER.info("HTTPServer listening on port http://localhost:" + server.getPort() + "/metrics");
+        LOGGER.info("HTTPServer listening on port http://"+inetAddress+" :" + server.getPort() + "/metrics");
     }
 
 }
